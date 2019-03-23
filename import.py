f99557ca1688bdc96174598bdbbcdd611f77113a
Carsten Niehaus <ni@kgs-rastede.de>
2019-03-23 18:40:27 +0100

    einlesen als Methode, Müll weg, Code besser strukturiert

--- import.py
+++ import.py
@@ -5,54 +5,60 @@ import csv
 
 from lehrer import *
 
-path = "lehrerdaten.CSV"
-
-file = open(path, newline='')
-
-reader = csv.reader(file, delimiter=";")
-
-header = next(reader)  # Die erste Zeile ist die Ueberschrift
-
-data = []
-
-for row in reader:
-
-    abteilungen = []
-    lernbefaehigungen = []
-
-    # Kuerzel, Vorname, Name, ABT1 -> ABT4, SOLL, IST-SOLL, FAECHER, LBF1, LBF2, LBF3
-    kuerzel = row[0]
-    vorname = row[1]
-    name = row[2]
-    abt1 = row[3]
-    abt2 = row[4]
-    abt3 = row[5]
-    abt4 = row[6]
-    abt5 = row[7]
-    soll = float(row[8])
-    ist = float(row[9])
-    faecher = row[10]
-    lbf1 = row[11]
-    lbf2 = row[12]
-    lbf3 = row[13]
-
-    abteilungen.append(abt1)
-    abteilungen.append(abt2)
-    abteilungen.append(abt3)
-    abteilungen.append(abt4)
-    abteilungen.append(abt5)
-
-    lernbefaehigungen.append(lbf1)
-    lernbefaehigungen.append(lbf2)
-    lernbefaehigungen.append(lbf3)
 
 
-    data.append([kuerzel, vorname, name, abt1, abt2, abt3, abt4,
-                 abt5, soll, ist, faecher, lbf1, lbf2, lbf3])
 
-    leh = Lehrer()
-    leh.setData(kuerzel, vorname, name, ist, soll, lernbefaehigungen, abteilungen)
+def dateiEinlesen():
+    liste = []
+
+    path = "lehrerdaten.CSV"
+
+    file = open(path, newline='')
+
+    reader = csv.reader(file, delimiter=";")
+
+    header = next(reader)  # Die erste Zeile ist die Ueberschrift
+
+    for row in reader:
+
+        abteilungen = []
+        lernbefaehigungen = []
+
+        # Name;Vorname;Nachname;Abt1;Abt2;Abt3;Abt4;Abt5;SOLL;Ist-Soll;LBF1;LBF2;LBF3;
+        kuerzel = row[0]
+        vorname = row[1]
+        name = row[2]
+        abt1 = row[3]
+        abt2 = row[4]
+        abt3 = row[5]
+        abt4 = row[6]
+        abt5 = row[7]
+        soll = float(row[8])
+        ist = float(row[9])
+        lbf1 = row[10]
+        lbf2 = row[11]
+        lbf3 = row[12]
+
+        abteilungen.append(abt1)
+        abteilungen.append(abt2)
+        abteilungen.append(abt3)
+        abteilungen.append(abt4)
+        abteilungen.append(abt5)
+
+        lernbefaehigungen.append(lbf1)
+        lernbefaehigungen.append(lbf2)
+        lernbefaehigungen.append(lbf3)
+
+        lehrer = Lehrer()
+        lehrer.setData(kuerzel, vorname, name, ist, soll, lernbefaehigungen, abteilungen)
+
+        liste.append(lehrer)
+
+    return liste
 
 
-# print(data[1])
-# print(data.sort())
+
+# alle Lehrer werden aus der CSV-Datei eingelesen und in der Liste gespeichert.
+# Danach liegen diese als LIste vor. Anleitung dazu hier:
+# https://docs.python.org/3/library/stdtypes.html#sequence-types-list-tuple-range
+lehrerliste = dateiEinlesen()
