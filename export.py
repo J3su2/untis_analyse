4c6c986dd442e67b3724b9f5caad730edf1906e7
Carsten Niehaus <ni@kgs-rastede.de>
2019-03-23 14:25:27 +0100

    Beispielcode zum Schreiben

--- export.py
+++ export.py
@@ -16,3 +16,13 @@ import csv
 # BB;-7.48
 # CCC;-11.88
 # AAA;-19.15
+
+
+# So geht es grundsaetzlich, in eine .csv-Datei zu schreiben. Die ersten
+# beiden Zeilen sind schon korrekt, die letzte Zeile muss ersetzt werden
+# durch eine eigenen Logik...
+#
+#
+#with open('minusstunden.csv', mode='w') as minusstunden_file:
+#    writer = csv.writer(minusstunden_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+#    writer.writerow(['DDD', '6.22'])
