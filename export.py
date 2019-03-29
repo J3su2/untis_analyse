95269f9804d8dfc9be53dff900e31b2d3ade598a
Carsten Niehaus <ni@kgs-rastede.de>
2019-03-29 12:22:50 +0100

    Rechtschreibfehler behoben

--- export.py
+++ export.py
@@ -1,6 +1,6 @@
 import csv
 
-# In dieser Datei sol pro Fach eine .csv-Datei generiert werden. Das bedeutet
+# In dieser Datei soll pro Fach eine .csv-Datei generiert werden. Das bedeutet
 # zumindestens fuer die erste Version fuer Mathe, Englisch, Biologie, Chemie und Physik
 #
 # Dabei sollen die Dateien ma.csv, en.csv, bi.csv, ch.csv und ph.csv entstehen
