3aef692d3d377e54be340c1974a8cf260fced728
Jesus_ <koala.panda@ewe.net>
2019-04-16 21:46:27 +0200

    Complete

--- 1Test.py
+++ 1Test.py
@@ -13,12 +13,3 @@ df = df.sort_values("Ist-Soll", ascending=False)
 df.to_csv("D:/Desktop/Projekt dv ni/lehrerdatenexport.csv", index=False)
 #Nochmal als .txt exportieren, diesmal mit ";" als Seperator. 
 df.to_csv("D:/Desktop/Projekt dv ni/lehrerdatenexport.txt", sep=";", index=False)
-
-
-#-------------------------------------Notizen----------------------------------------------------------------------------------------
-#print(df)
-#print (df.columns)
-#print (df[["Name", "Ist-Soll"]][0:5]) 
-#print(df.describe())
-#print(df.sort_values("Ist-Soll", ascending=False))
-
