ba6516a87b2a303f7da5f6508da3e31f88f8c9eb
Jesus_ <49657785+J3su2@users.noreply.github.com>
2019-04-18 13:48:03 +0200

    Update 1Test.py

--- 1Test.py
+++ 1Test.py
@@ -2,7 +2,7 @@
 import pandas as pd 
 
 #Einlesen der Datei / Abstandhalter definieren / encoding definieren (muss nicht, glaube ich macht OpenOffice automatisch. Nur wenn man die Datei noch nie geöffnet hat)
-df = pd.read_csv("D:/Desktop/Projekt dv ni/lehrerdaten.csv", delimiter=";", encoding = "ISO-8859-1")
+df = pd.read_csv("D:/Desktop/Projekt_dv_ni/lehrerdaten.csv", delimiter=";", encoding = "ISO-8859-1")
 
 #Zuerst auf die beiden Spalten reduzieren
 df = df[["Name", "Ist-Soll"]]
@@ -10,6 +10,6 @@ df = df[["Name", "Ist-Soll"]]
 df = df.sort_values("Ist-Soll", ascending=False)
 
 #als .csv Datei exportieren, ohne Auto-Index / (Seperator = ",")
-df.to_csv("D:/Desktop/Projekt dv ni/lehrerdatenexport.csv", index=False)
+df.to_csv("D:/Desktop/Projekt_dv_ni/lehrerdatenexport.csv", index=False)
 #Nochmal als .txt exportieren, diesmal mit ";" als Seperator. 
-df.to_csv("D:/Desktop/Projekt dv ni/lehrerdatenexport.txt", sep=";", index=False)
+df.to_csv("D:/Desktop/Projekt_dv_ni/lehrerdatenexport.txt", sep=";", index=False)
